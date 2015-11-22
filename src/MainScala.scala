c52a25eb0e7194777353ab1fa42e2a2c8ddd5781
VoronaPavel <vorona.pavel.nickolaevich@gmail.com>
2015-11-22 12:36:54 +0200

    polishing

--- src/MainScala.scala
+++ src/MainScala.scala
@@ -5,22 +5,22 @@ object MainScala extends App {
     def closestJump: Long = {
 
       def search(move : (Long => Long), point: Long = to, distance : Long = 1) : (Long, Long) =
-        if (!containsDigits(point)) (point, distance)
+        if (notContainsDigits(point)) (point, distance)
         else search(move, move(point), distance + 1)
 
-      if (!containsDigits(to))
+      if (notContainsDigits(to))
         to
       else {
-        def left = (n : Long) => n - 1
-        def right = (n : Long) => n + 1
+        def left(n : Long) = n - 1
+        def right(n : Long) = n + 1
         val (leftPoint, leftDistance) = search(left)
         val (rightPoint, rightDistance) = search(right)
         if (leftDistance < rightDistance) leftPoint else rightPoint
       }
     }
 
-    def containsDigits(number: Long) =
-      digits.toStream.exists(digit => number.toString.contains(digit.toString))
+    def notContainsDigits(number: Long) =
+      !digits.toStream.exists(digit => number.toString.contains(digit.toString))
 
     def distance(from : Long, to : Long) =
       if (from > to) from - to else to - from
@@ -30,20 +30,4 @@ object MainScala extends App {
 
     if (stepsWithJump > stepsByStep) stepsByStep else stepsWithJump
   }
-
-  val excluded1: Array[Int] = Array(1)
-  val excluded2: Array[Int] = Array(9)
-  val excluded3: Array[Int] = Array(9, 8)
-  val excluded4: Array[Int] = Array(9, 8, 7, 6, 5, 4, 3, 2)
-
-  val solve1 = steps(0, 10, excluded1)
-  val solve2 = steps(100, 95, excluded2)
-  val solve3 = steps(100, 90, excluded3)
-  val solve4 = steps(100, 90, excluded4)
-
-  println(2 == solve1)
-  println(5 == solve2)
-  println(10 == solve3)
-  println(10 == solve4)
-
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -3,6 +3,23 @@ import org.junit.Test;
 
 public class MainTest {
 
+
+
+//    val excluded1: Array[Int] = Array(1)
+//    val excluded2: Array[Int] = Array(9)
+//    val excluded3: Array[Int] = Array(9, 8)
+//    val excluded4: Array[Int] = Array(9, 8, 7, 6, 5, 4, 3, 2)
+//
+//    val solve1 = steps(0, 10, excluded1)
+//    val solve2 = steps(100, 95, excluded2)
+//    val solve3 = steps(100, 90, excluded3)
+//    val solve4 = steps(100, 90, excluded4)
+//
+//    println(2 == solve1)
+//    println(5 == solve2)
+//    println(10 == solve3)
+//    println(10 == solve4)
+
     @Test
     public void testSolve() throws Exception {
         int [] excluded = {1};
